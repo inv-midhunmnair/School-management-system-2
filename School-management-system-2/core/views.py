9173387293576a7ca25f2923f29ad5203dd34287
inv-midhunmnair <midhun.nair@innovaturelabs.com>
2025-07-21 17:30:38 +0530

    fix: minor fix regarding jwt auth

--- School-management-system-2/core/views.py
+++ School-management-system-2/core/views.py
@@ -1,3 +1,4 @@
+import os
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import CustomTokenObtainPairSerializer
 from rest_framework.views import APIView
--- School-management-system-2/student_mgmt/settings.py
+++ School-management-system-2/student_mgmt/settings.py
@@ -66,6 +66,18 @@ INSTALLED_APPS = [
     'corsheaders'
 ]
 
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=45),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
+
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
